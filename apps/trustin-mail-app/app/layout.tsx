fb211291fe187ec68b40fe391b857e6803ace9ad
Anas SAHEL <anas.sahel@gmail.com>
2024-02-15 11:33:32 +0000

    fix: no need to export font

--- apps/trustin-mail-app/app/layout.tsx
+++ apps/trustin-mail-app/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next';
 import { Roboto as FontSans } from 'next/font/google';
 import './globals.css';
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ['latin'],
   weight: ['100', '300', '400', '500', '700', '900'],
   variable: '--font-sans',
