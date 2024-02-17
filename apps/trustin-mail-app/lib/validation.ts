d39e03e115a00511c11087b93b983e0d9cd77c2c
Anas SAHEL <anas.sahel@gmail.com>
2024-02-17 17:04:26 +0000

    feat: Add registration

---
+++ apps/trustin-mail-app/lib/validation.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const loginSchema = z
+  .object({
+    email: z
+      .string()
+      .min(1, { message: 'Email is required' })
+      .email({ message: 'Invalid email address' }),
+    password: z
+      .string()
+      .min(1, { message: 'Password is required' })
+      .min(6, { message: 'Password must be at least 6 characters' }),
+  })
+  .required();
+export type LoginSchemaType = z.infer<typeof loginSchema>;
+
+export const registerSchema = loginSchema.extend({});
+export type RegisterSchemaType = z.infer<typeof registerSchema>;
