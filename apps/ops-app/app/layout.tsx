82b70f41ade12e6e3d640c8423b37486f617c35c
Anas SAHEL <anas.sahel@gmail.com>
2024-03-19 09:46:50 +0000

    feat: setup shadcn

--- apps/ops-app/app/layout.tsx
+++ apps/ops-app/app/layout.tsx
@@ -1,8 +1,9 @@
+import { cn } from '@/lib/utils';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const fontSans = FontSans({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
   title: 'Ops App',
@@ -12,8 +13,14 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          'bg-background min-h-screen font-sans antialiased',
+          fontSans.variable
+        )}>
+        {children}
+      </body>
     </html>
   );
 }
